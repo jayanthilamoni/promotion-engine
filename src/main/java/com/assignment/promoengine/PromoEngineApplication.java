7dceaffd0cb77c86a0b4c3390681ffbd7759ffa1
jayanth_ilamoni <ijayanth@deloitte.com>
2020-12-19 21:50:28 +0530

    Initial Commit

---
+++ src/main/java/com/assignment/promoengine/PromoEngineApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.promoengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/promoengine/PromoEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.promoengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
