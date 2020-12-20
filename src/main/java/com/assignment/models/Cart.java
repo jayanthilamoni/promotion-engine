fede2cfbf146ebd3ccb5cf24b8cc6a99e0d24721
jayanth_ilamoni <ijayanth@deloitte.com>
2020-12-20 13:28:12 +0530

    Initiating cart entries by default

--- src/main/java/com/assignment/models/Cart.java
+++ src/main/java/com/assignment/models/Cart.java
@@ -3,6 +3,7 @@ package com.assignment.models;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,6 +12,10 @@ public class Cart {
     private UUID cartId;
     private List<CartEntry> cartEntries;
     private double discount;
+
+    public Cart() {
+        cartEntries = new ArrayList<>();
+    }
 
     public UUID getCartId() {
         return cartId;
