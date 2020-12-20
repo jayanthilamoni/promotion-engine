d9c70f3637000a6f4479acec5c4041176aa5cf33
jayanth_ilamoni <ijayanth@deloitte.com>
2020-12-20 23:04:51 +0530

    Adding test class for Promotion Controller

--- src/test/java/com/assignment/controllertest/PromotionControllerTest.java
+++ src/test/java/com/assignment/controllertest/PromotionControllerTest.java
@@ -1,5 +1,26 @@
 package com.assignment.controllertest;
 
+import com.assignment.controllers.PromotionsController;
+import com.assignment.dba.PromotionDB;
+import com.assignment.models.promotion.PromotionEntryForm;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 public class PromotionControllerTest {
-    
+    private final PromotionsController promotionsController;
+    PromotionControllerTest(){
+        PromotionDB promotionDB = PromotionDB.getInstance();
+        promotionsController = new PromotionsController(promotionDB);
+    }
+    @Test
+    public void whenCorrectTypeOnePromo_addPromotion(){
+        PromotionEntryForm promotionEntryForm = new PromotionEntryForm();
+        promotionEntryForm.setPromotionType(1);
+        promotionEntryForm.setMultiple(4);
+        promotionEntryForm.setOfferPrice(120);
+        promotionEntryForm.setProductA("A");
+        String response = promotionsController.addPromotion(promotionEntryForm);
+        assertTrue(response.contains("Promotion with ID "));
+    }
 }
